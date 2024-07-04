191aa5b0e509bb165cb7446301c4baf3ea33e85d
Dominik Černjević <dcernjevi21@foi.hr>
2024-07-05 00:18:53 +0200

    Poboljsanja vezane za kretanje igraca

--- code/main.py
+++ code/main.py
@@ -2,47 +2,87 @@ import pygame
 
 pygame.init()
 
+#max frame rate
+clock = pygame.time.Clock()
+fps = 60
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #game window size
 pygame.display.set_caption('Igrica')
 
+moving_left = False
+moving_right = False
 
-class Player(pygame.sprite.Sprite):
-	def __init__(self, x, y, scale): #constructor	
+background_color = (0, 255, 255)
+
+def draw_Background():
+	screen.fill(background_color)
+
+class Character(pygame.sprite.Sprite):
+	def __init__(self, character_type, x, y, scale, speed): #constructor	
 		pygame.sprite.Sprite.__init__(self)
-		playerImg = pygame.image.load('../images/Player/Idle/E_E_Gun__Idle_000.png') #loading the player image
+		self.character_type = character_type
+		self.speed = speed #in pixels
+		self.direction = 1
+		self.flip = False
+		playerImg = pygame.image.load(f'../images/{self.character_type}/Idle/0.png') #loading the character image (player, soldier...)
 		self.playerImg = pygame.transform.scale(playerImg, (int(playerImg.get_width() * scale), (int(playerImg.get_height() * scale))))
 		self.rect = self.playerImg.get_rect() #rectangle for player character
 		self.rect.center = (x, y) #position the character on a certain position of the game window
 
-	def draw(self):
-		screen.blit(self.playerImg, self.rect)
+	def move(self, moving_left, moving_right):
+		dx = 0 #change in the x position
+		dy = 0 #change in the y position
+		
+		#move 
+		if moving_left:
+			dx = -self.speed
+			self.flip = True
+			self.direction = -1
+		
+		if moving_right:
+			dx = self.speed
+			self.flip = False
+			self.direction = 1
+		
+		#change player position
+		self.rect.x += dx
+		self.rect.y += dy
 
-player = Player(200, 200, 0.15)
+	def draw(self):
+		screen.blit(pygame.transform.flip(self.playerImg,  self.flip, False), self.rect)
+
+
+player = Character('player', 200, 200, 0.15, 5)
 
 run = True
 while run: #loop for running the game
 
+	clock.tick(fps)
+	
+	draw_Background()
 	player.draw()
-	
-	key = pygame.key.get_pressed() #checking if a button is pressed
-	if key[pygame.K_a] == True:
-		playerImg.move_ip(-1, 0)
-	elif key[pygame.K_d] == True:
-		playerImg.move_ip(1, 0)
-	elif key[pygame.K_w] == True:
-		playerImg.move_ip(0, -1)
-	elif key[pygame.K_s] == True:
-		playerImg.move_ip(0, 1)
-	
-	if key[pygame.K_ESCAPE] == True: #quiting the game with ESC button
-		pygame.quit()
+	player.move(moving_left, moving_right)
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			run = False
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_a:
+				moving_left = True
+			if event.key == pygame.K_d:
+				moving_right = True
+			if event.key == pygame.K_ESCAPE:
+				run = False
+		
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_a:
+				moving_left = False
+			if event.key == pygame.K_d:
+				moving_right = False
+			
 
 	pygame.display.update()
 
