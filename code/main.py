71edb7c1ddfe64e804ef34779c17677672de8301
Dominik Černjević <dcernjevi21@foi.hr>
2024-07-04 23:02:30 +0200

    Dodana klasa za igraca te prikaz slike igraca na zaslonu

--- code/main.py
+++ code/main.py
@@ -6,25 +6,39 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Igrica')
 
-player = pygame.Rect((300, 250, 50, 50)) #player 'object'
+
+class Player(pygame.sprite.Sprite):
+	def __init__(self, x, y, scale): #constructor	
+		pygame.sprite.Sprite.__init__(self)
+		playerImg = pygame.image.load('../images/Player/Idle/E_E_Gun__Idle_000.png') #loading the player image
+		self.playerImg = pygame.transform.scale(playerImg, (int(playerImg.get_width() * scale), (int(playerImg.get_height() * scale))))
+		self.rect = self.playerImg.get_rect() #rectangle for player character
+		self.rect.center = (x, y) #position the character on a certain position of the game window
+
+	def draw(self):
+		screen.blit(self.playerImg, self.rect)
+
+player = Player(200, 200, 0.15)
 
 run = True
 while run: #loop for running the game
+
+	player.draw()
 	
-	screen.fill((0, 0, 0))	
-
-	pygame.draw.rect(screen, (255, 0, 0), player)
-
 	key = pygame.key.get_pressed() #checking if a button is pressed
 	if key[pygame.K_a] == True:
-		player.move_ip(-1, 0)
+		playerImg.move_ip(-1, 0)
 	elif key[pygame.K_d] == True:
-		player.move_ip(1, 0)
+		playerImg.move_ip(1, 0)
 	elif key[pygame.K_w] == True:
-		player.move_ip(0, -1)
+		playerImg.move_ip(0, -1)
 	elif key[pygame.K_s] == True:
-		player.move_ip(0, 1)
+		playerImg.move_ip(0, 1)
+	
+	if key[pygame.K_ESCAPE] == True: #quiting the game with ESC button
+		pygame.quit()
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
